1e9726960219e02051678524664850e4c60bb3cf
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2024-07-19 14:02:51 +0100

    Improved file naming

--- src/main.rs
+++ src/main.rs
@@ -28,9 +28,9 @@ pub enum State {
 enum Command {
     #[command(description = "display this text.")]
     Help,
-    #[command(description = "synth a text in portuguese.")]
+    #[command(description = "speak a text in portuguese.")]
     Portuguese,
-    #[command(description = "synth a text in polish.")]
+    #[command(description = "speak a text in polish.")]
     Polish,
 }
 
@@ -54,12 +54,12 @@ async fn main() {
                                 .await?;
                         }
                         Command::Portuguese => {
-                            bot.send_message(msg.chat.id, "Send me the plain text in Portuguese")
+                            bot.send_message(msg.chat.id, "Send me a plain text in portuguese")
                                 .await?;
                             dialogue.update(State::ReceiveTextInPortuguese).await?;
                         }
                         Command::Polish => {
-                            bot.send_message(msg.chat.id, "Send me the plain text in Portuguese")
+                            bot.send_message(msg.chat.id, "Send me a plain text in polish")
                                 .await?;
                             dialogue.update(State::ReceiveTextInPolish).await?;
                         }
@@ -87,12 +87,13 @@ async fn main() {
                             Some(text) => {
                                 let audio = synth.synth(text, &Portuguese).await?;
                                 let bytes = bytes::Bytes::from(audio);
-                                let file = InputFile::memory(bytes).file_name("audio.mp3");
+                                let file = InputFile::memory(bytes)
+                                    .file_name(format!("{}.pt.mp3", &text[0..20]));
                                 bot.send_audio(msg.chat.id, file).await?;
                                 dialogue.update(State::Start).await?;
                             }
                             None => {
-                                bot.send_message(msg.chat.id, "Send me a plain text in Portuguese")
+                                bot.send_message(msg.chat.id, "Send me a plain text in portuguese")
                                     .await?;
                             }
                         }
@@ -112,13 +113,14 @@ async fn main() {
                             Some(text) => {
                                 let audio = synth.synth(text, &Polish).await?;
                                 let bytes = bytes::Bytes::from(audio);
-                                let file = InputFile::memory(bytes).file_name("audio.mp3");
+                                let file = InputFile::memory(bytes)
+                                    .file_name(format!("{}.pl.mp3", &text[0..20]));
 
                                 bot.send_audio(msg.chat.id, file).await?;
                                 dialogue.update(State::Start).await?;
                             }
                             None => {
-                                bot.send_message(msg.chat.id, "Send me a plain text in Polish")
+                                bot.send_message(msg.chat.id, "Send me a plain text in polish")
                                     .await?;
                             }
                         }
